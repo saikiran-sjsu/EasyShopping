f38310946aa1123b16d18a9d3e3e20cb6125996c
Brandon Cheu <54514627+b1820990@users.noreply.github.com>
2020-04-23 14:13:11 -0700

    Add files via upload

--- ShoppingApp/__init__.py
+++ ShoppingApp/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'SECRET_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sai@localhost:5432/testing'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-from ShoppingApp import routes
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+from ShoppingApp import routes , models
+
+
--- ShoppingApp/routes.py
+++ ShoppingApp/routes.py
@@ -1,7 +1,8 @@
-from flask import app
 from flask import render_template
-from ShoppingApp import app
-
+from ShoppingApp import app, db
+from ShoppingApp.forms import RegisterForm, LoginForm
+from ShoppingApp.models import User, Items
+from flask_login import login_user, logout_user
 
 @app.route('/')
 @app.route('/home')
@@ -9,17 +10,49 @@ def home():
     title = 'Home'
     return render_template('home.html', title=title)
 
+
 @app.route('/product')
 def high():
     title = 'products'
     return render_template('product.html', title=title)
+
 
 @app.route('/cart')
 def mid():
     title = 'cart'
     return render_template('cart.html', title=title)    
 
+
 @app.route('/wishlist')
 def low():
     title = 'wishlist'
-    return render_template('wishlist.html', title=title)        
+    return render_template('wishlist.html', title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = 'Login'
+    form = LoginForm()
+    #if form.validate_on_submit():
+
+
+
+    return render_template('login.html', title=title, form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    title = 'Register'
+    form = RegisterForm()
+    if form.validate_on_submit():
+        users = User(userName=form.userName.data, firstName=form.firstName.data,
+                     lastName=form.lastName.data, email=form.email.data, CreditCard=form.CreditCard.data,
+                     ccv=form.ccv.data)
+        users.set_password(form.password.data)
+        users.set_secret_key(form.secret_key.data)
+        db.session.add(users)
+        db.session.commit()
+        print("Account Created!")
+
+
+    return render_template('register.html', title=title, form=form)
